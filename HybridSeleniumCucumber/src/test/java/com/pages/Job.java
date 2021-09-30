b951eedb4ddb1db06d8af11a9e83b56515883e00
ADMIN <ADMIN@ADMIN-PC>
2021-09-30 16:48:25 +0530

    Commit HybridSeleniumCucumber

---
+++ HybridSeleniumCucumber/src/test/java/com/pages/Job.java
@@ -0,0 +1,11 @@
+package com.pages;
+
+public class Job extends Library {
+	
+	
+	
+	
+	
+	
+
+}
---
+++ HybridSeleniumCucumber/src/test/java/com/testCase/LoginTestCase.java
@@ -0,0 +1,36 @@
+package com.testCase;
+import java.io.IOException;
+
+
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.pages.LoginPage;
+import com.seleniumReusableFunctions.SeleniumUtilities;
+import com.baseClass.Library;
+
+public class LoginTestCase extends Library {
+	@BeforeTest
+    public void stratUp() {
+        launchApplication("chrome","https://opensource-demo.orangehrmlive.com/");
+       
+    }
+    @Test
+    public void login() {
+        LoginPage lpage=new LoginPage(driver);
+        lpage.login("Admin", "admin123");
+       
+    }
+    @AfterClass
+    public void close() throws IOException {
+        SeleniumUtilities sUti = new SeleniumUtilities(driver);
+        sUti.to_take_screenshot("F:\\LTI_SOFT\\eclipse-workspace\\HybridSeleniumCucumber\\src\\test\\resources\\ScreenShots\\orangeHRMLogin.png");
+       
+        quit();
+       
+    }
+ 
+
+}
